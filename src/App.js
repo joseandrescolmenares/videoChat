076c5f63518920cbe4f500791e9bbdbff00fa6b9
jose <joseandrescolmenares02@gmail.com>
2022-07-26 13:18:53 -0300

    primer

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import TextField from "@material-ui/core/TextField"
+import AssignmentIcon from "@material-ui/icons/Assignment"
+import PhoneIcon from "@material-ui/icons/Phone"
+import React, { useEffect, useRef, useState } from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import Peer from "simple-peer"
+import io from "socket.io-client"
+import "./App.css"
+const socket = io.connect('http://localhost:5000')
+function App() {
+
+  const [ me, setMe ] = useState("")
+	const [ stream, setStream ] = useState()
+	const [ receivingCall, setReceivingCall ] = useState(false)
+	const [ caller, setCaller ] = useState("")
+	const [ callerSignal, setCallerSignal ] = useState()
+	const [ callAccepted, setCallAccepted ] = useState(false)
+	const [ idToCall, setIdToCall ] = useState("")
+	const [ callEnded, setCallEnded] = useState(false)
+	const [ name, setName ] = useState("")
+	const myVideo = useRef()
+	const userVideo = useRef()
+	const connectionRef= useRef()
+
+useEffect(() => {
+  navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((stream) => {
+    setStream(stream)
+    myVideo.current.srcObject = stream
+  })
+},[])
+
+const callUser = (id) => {
+  const peer = new Peer({
+    initiator: true,
+    trickle: false,
+    stream: stream
+  })
+  peer.on("signal", (data) => {
+    socket.emit("callUser", {
+      userToCall: id,
+      signalData: data,
+      from: me,
+      name: name
+    })
+  })
+
+socket.on("me", (id) => {
+  setMe(id)
+})
+
+socket.on("callUser", (data) => {
+  setReceivingCall(true)
+  setCaller(data.from)
+  setName(data.name)
+  setCallerSignal(data.signal)
+})
+
+
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
